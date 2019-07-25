38d6c086ff36f27aa9a15b9b85f9f97241921e7e
Bill Glick <bglick@illinois.edu>
2019-07-25 15:42:54 -0500

    Start to merge in datacenter_cfg

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,10 +7,25 @@
 # @example
 #   include baseline_cfg
 class baseline_cfg {
+
+    ## ALL SYSTEMS INCLUDING CONTAINERS
     include ::baseline_cfg::additional_packages
     include ::baseline_cfg::email
     include ::baseline_cfg::motd
     include ::baseline_cfg::puppet
-    include ::baseline_cfg::selinux
-    include ::baseline_cfg::time
+
+    ## NON-CONTAINER CONFIGURATION
+    ## MAY NEED TO ADD ADDITIONAL CONTAINER TYPES BELOW
+    if ( $::virtual !~ /docker/ )
+    {
+#        include ::baseline_cfg::disk
+#        include ::baseline_cfg::dns_resolv
+#        include ::baseline_cfg::network
+        include ::baseline_cfg::selinux
+#        include ::baseline_cfg::syslog
+        include ::baseline_cfg::time
+#        include ::baseline_cfg::vmtools
+#        include ::baseline_cfg::yum_repo
+    }
+
 }
